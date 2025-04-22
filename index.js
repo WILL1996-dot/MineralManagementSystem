f895fb97bc311a6ac0e6e7dda3959936ae92cf1d
William D Cooper <cwilloam7@gmail.com>
2025-04-22 23:55:14 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = "https://mudplvaigphenzayycsl.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im11ZHBsdmFpZ3BoZW56YXl5Y3NsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUzNjMxODEsImV4cCI6MjA2MDkzOTE4MX0.BQWNKTX1oqdoPlHjFIVik9hEi5BdsDYbHwvC19mbJxg";
 const supabase = supabase.createClient(supabaseUrl, supabaseKey);
