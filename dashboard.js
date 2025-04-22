859473708d21c252420344725ea3ce9cac17e965
William D Cooper <cwilloam7@gmail.com>
2025-04-22 23:35:41 +0000

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,1 +1,17 @@
-cncn
+// Sign up
+async function signUp(email, password) {
+  const { user, error } = await supabase.auth.signUp({ email, password });
+  console.log(user, error);
+}
+
+// Login
+async function login(email, password) {
+  const { user, error } = await supabase.auth.signInWithPassword({ email, password });
+  console.log(user, error);
+}
+
+// Logout
+async function logout() {
+  const { error } = await supabase.auth.signOut();
+  if (!error) alert("Logged out!");
+}
